ccccddcb3b96db28687ae7e459dec2213c9c4273
Alexa Guerrero <alexa.guerrero@maine.edu>
2025-03-06 17:41:47 -0500

    Updated my react and added a side navigation bar
    This side navigation bar is in it's own folder called 'components,' which can be attached to a page or removed since it is on it's own. The nav-bar is connected to FakePage.js to how it can be used for a page file.

--- Academic-Notification/src/App.js
+++ Academic-Notification/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Selection from "./Pages/Selection";
 import DegreeProgress from "./Pages/DegreeProgress";
+import FakePage from "./Pages/FakePage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Selection />} />
         <Route path="/degree-progress/:studentID" element={<DegreeProgress />} />
+        <Route path="/fake-page" element={<FakePage />} />
       </Routes>
     </Router>
   );
--- Academic-Notification/src/Pages/DegreeProgress.js
+++ Academic-Notification/src/Pages/DegreeProgress.js
@@ -34,7 +34,7 @@ export default function DegreeProgress() {
                     <h3>Majors & Minors</h3>
                     <div className='major'>
                         <span>Major: Electrical Engineerig</span>
-                        <button className="credit-report">Credit Report</button>
+                        <button className="credit-report" onClick={() => navigate("/fake-page")}>Credit Report</button>
                     </div>
                     <div className='major'>
                         <span>Major: Chemical Engineerig</span>
