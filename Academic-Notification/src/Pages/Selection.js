22ae6f26b36a811400dc5afbedad173213bc709c
KevinM <kevinm246816@gmail.com>
2025-03-31 13:17:12 -0400

    Strip outdated selection list

--- Academic-Notification/src/Pages/Selection.js
+++ Academic-Notification/src/Pages/Selection.js
@@ -6,38 +6,9 @@ import { FiSettings } from 'react-icons/fi';
 
 export default function Selection() {
   const [search, setSearch] = useState('');
-  const [data, setData] = useState([]);
-  const [selectedIndex, setSelectedIndex] = useState(-1); // Tracks highlighted <li>
   const navigate = useNavigate();
-  const listRefs = useRef([]); // Store refs for list items
 
-  useEffect(() => {
-    const fetchStudentData = async () => {
-      try {
-        const studentFiles = [
-          '1125662', '0000000', '1122334', '1634455',
-          '1182072', '1011854', '9876543', '1011856',
-        ];
-        const students = [];
-        for (let file of studentFiles) {
-          const response = await fetch(`/students/${file}.json`);
-          if (response.ok) {
-            const studentData = await response.json();
-            students.push({ id: file, data: studentData });
-          } else {
-            console.error(`Failed to fetch student data for ${file}`);
-          }
-        }
-        setData(students);
-      } catch (error) {
-        console.error('Error fetching student data:', error);
-      }
-    };
-    fetchStudentData();
-  }, []);
-
-
-  const filteredData = search ? data.filter((item) => item.id.includes(search)) : [];
+  useEffect(() => {}, []);
 
   const handleSelectStudent = (studentID) => {
     
@@ -52,25 +23,7 @@ export default function Selection() {
       handleSelectStudent(search);
     
     }
-
-    if (e.key === 'Tab' && filteredData.length > 0) {
-      e.preventDefault(); 
-
-      if (e.shiftKey) {
-        setSelectedIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : filteredData.length - 1));
-      } else {
-
-        setSelectedIndex((prevIndex) => (prevIndex < filteredData.length - 1 ? prevIndex + 1 : 0));
-      }
-    }
   };
-
-
-  useEffect(() => {
-    if (selectedIndex !== -1 && listRefs.current[selectedIndex]) {
-      listRefs.current[selectedIndex].focus();
-    }
-  }, [selectedIndex]);
 
   return (
     <div className="selections">
